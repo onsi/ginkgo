fc845adfda4e8d7f3f5899359969e569d4e63d8e
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2013-08-27 09:06:09 -0700

    added test coverage for runnable nodes

---
+++ runnable_node_test.go
@@ -0,0 +1,163 @@
+package ginkgo
+
+import (
+	. "github.com/onsi/gomega"
+	"runtime"
+	"time"
+)
+
+func init() {
+	Describe("RunnableNode", func() {
+		Describe("basic construction parameters", func() {
+			It("should store off the passed in code location", func() {
+				codeLocation := generateCodeLocation(0)
+				Ω(newRunnableNode(func() {}, codeLocation, 0).codeLocation).Should(Equal(codeLocation))
+			})
+		})
+
+		Describe("running the passed in function", func() {
+			Context("when the function is synchronous and does not panic", func() {
+				It("should run the function and report a runOutcomeCompleted", func() {
+					didRun := false
+					runnableNode := newRunnableNode(func() {
+						didRun = true
+					}, generateCodeLocation(0), 0)
+
+					outcome, failure := runnableNode.run()
+
+					Ω(didRun).Should(BeTrue())
+					Ω(outcome).Should(Equal(runOutcomeCompleted))
+					Ω(failure).Should(BeZero())
+				})
+			})
+
+			Context("when the function is synchronous and *does* panic", func() {
+				var (
+					codeLocation CodeLocation
+					outcome      runOutcome
+					failure      failureData
+				)
+
+				BeforeEach(func() {
+					node := newRunnableNode(func() {
+						codeLocation = generateCodeLocation(0)
+						panic("ack!")
+					}, generateCodeLocation(0), 0)
+
+					outcome, failure = node.run()
+				})
+
+				It("should run the function and report a runOutcomePanicked", func() {
+					Ω(outcome).Should(Equal(runOutcomePanicked))
+					Ω(failure.message).Should(Equal("Test Panicked"))
+				})
+
+				It("should include the code location of the panic itself", func() {
+					Ω(failure.codeLocation.FileName).Should(Equal(codeLocation.FileName))
+					Ω(failure.codeLocation.LineNumber).Should(Equal(codeLocation.LineNumber + 1))
+				})
+
+				It("should include the panic data", func() {
+					Ω(failure.forwardedPanic).Should(Equal("ack!"))
+				})
+			})
+
+			Context("when the function is asynchronous", func() {
+				var (
+					node               *runnableNode
+					sleepDuration      time.Duration
+					timeoutDuration    time.Duration
+					numberOfGoRoutines int
+					sendToChannel      bool
+				)
+
+				BeforeEach(func() {
+					sleepDuration = time.Duration(0.001 * float64(time.Second))
+					timeoutDuration = time.Duration(1 * float64(time.Second))
+					sendToChannel = true
+				})
+
+				JustBeforeEach(func() {
+					node = newRunnableNode(func(done Done) {
+						numberOfGoRoutines = runtime.NumGoroutine()
+						time.Sleep(sleepDuration)
+						if sendToChannel {
+							done <- true
+						}
+					}, generateCodeLocation(0), timeoutDuration)
+				})
+
+				It("should run the function as a goroutine", func() {
+					initialNumberOfGoRoutines := runtime.NumGoroutine()
+					outcome, failure := node.run()
+
+					Ω(outcome).Should(Equal(runOutcomeCompleted))
+					Ω(failure).Should(BeZero())
+
+					Ω(numberOfGoRoutines).Should(Equal(initialNumberOfGoRoutines + 1))
+				})
+
+				Context("when the function takes longer than the timeout", func() {
+					BeforeEach(func() {
+						sleepDuration = time.Duration(0.002 * float64(time.Second))
+						timeoutDuration = time.Duration(0.001 * float64(time.Second))
+					})
+
+					It("should timeout", func() {
+						outcome, failure := node.run()
+						Ω(outcome).Should(Equal(runOutcomeTimedOut))
+						Ω(failure.message).Should(Equal("Timed out"))
+						Ω(failure.codeLocation).Should(Equal(node.codeLocation))
+					})
+				})
+
+				Context("when the function fails to send to the done channel", func() {
+					BeforeEach(func() {
+						sleepDuration = time.Duration(0.0005 * float64(time.Second))
+						timeoutDuration = time.Duration(0.001 * float64(time.Second))
+						sendToChannel = false
+					})
+
+					It("should timeout", func() {
+						outcome, failure := node.run()
+						Ω(outcome).Should(Equal(runOutcomeTimedOut))
+						Ω(failure.message).Should(Equal("Timed out"))
+						Ω(failure.codeLocation).Should(Equal(node.codeLocation))
+					})
+				})
+
+			})
+
+			Context("when the function takes the wrong kind of argument", func() {
+				It("should panic", func() {
+					Ω(func() {
+						newRunnableNode(func(oops string) {
+						}, generateCodeLocation(0), 0)
+					}).Should(Panic())
+				})
+			})
+
+			Context("when the function takes more than one argument", func() {
+				It("should panic", func() {
+					Ω(func() {
+						newRunnableNode(func(done Done, oops string) {
+						}, generateCodeLocation(0), 0)
+					}).Should(Panic())
+				})
+			})
+		})
+	})
+
+	Describe("ItNodes", func() {
+		It("should save off the text and flags", func() {
+			didRun := false
+			codeLocation := generateCodeLocation(0)
+			it := newItNode("my it node", func() {
+				didRun = true
+			}, flagTypeFocused, codeLocation, 0)
+			Ω(it.flag).Should(Equal(flagTypeFocused))
+			Ω(it.text).Should(Equal("my it node"))
+			Ω(it.codeLocation).Should(Equal(codeLocation))
+		})
+	})
+}
