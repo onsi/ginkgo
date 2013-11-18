e6c0dc0804352dad17f2e8df1b75c3b1fca0fe39
Maria Shaldibina and Onsi Fakhouri <pair+mariash+onsi@pivotallabs.com>
2013-11-18 14:40:23 -0800

    added junit reporter

---
+++ reporters/junit_reporter.go
@@ -0,0 +1,102 @@
+package reporters
+
+import (
+	"encoding/xml"
+	"fmt"
+	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/types"
+	"os"
+	"strings"
+)
+
+type JUnitTestSuite struct {
+	XMLName   xml.Name        `xml:"testsuite"`
+	TestCases []JUnitTestCase `xml:"testcase"`
+	Tests     int             `xml:"tests,attr"`
+	Failures  int             `xml:"failures,attr"`
+	Time      float64         `xml:"time,attr"`
+}
+
+type JUnitTestCase struct {
+	Name           string               `xml:"name,attr"`
+	ClassName      string               `xml:"classname,attr"`
+	FailureMessage *JUnitFailureMessage `xml:"failure,omitempty"`
+	Skipped        *JUnitSkipped        `xml:"skipped,omitempty"`
+	Time           float64              `xml:"time,attr"`
+}
+
+type JUnitFailureMessage struct {
+	Type    string `xml:"type,attr"`
+	Message string `xml:",chardata"`
+}
+
+type JUnitSkipped struct {
+	XMLName xml.Name `xml:"skipped"`
+}
+
+type JUnitReporter struct {
+	suite         JUnitTestSuite
+	filename      string
+	testSuiteName string
+}
+
+func NewJUnitReporter(filename string) *JUnitReporter {
+	return &JUnitReporter{
+		filename: filename,
+	}
+}
+
+func (reporter *JUnitReporter) SpecSuiteWillBegin(config config.GinkgoConfigType, summary *types.SuiteSummary) {
+	reporter.suite = JUnitTestSuite{
+		Tests:     summary.NumberOfExamplesThatWillBeRun,
+		TestCases: []JUnitTestCase{},
+	}
+	reporter.testSuiteName = summary.SuiteDescription
+}
+
+func (reporter *JUnitReporter) ExampleWillRun(exampleSummary *types.ExampleSummary) {
+}
+
+func (reporter *JUnitReporter) ExampleDidComplete(exampleSummary *types.ExampleSummary) {
+	testCase := JUnitTestCase{
+		Name:      strings.Join(exampleSummary.ComponentTexts[1:], " "),
+		ClassName: reporter.testSuiteName,
+	}
+	if exampleSummary.State == types.ExampleStateFailed || exampleSummary.State == types.ExampleStateTimedOut || exampleSummary.State == types.ExampleStatePanicked {
+		failureType := ""
+		switch exampleSummary.State {
+		case types.ExampleStateFailed:
+			failureType = "Failure"
+		case types.ExampleStateTimedOut:
+			failureType = "Timeout"
+		case types.ExampleStatePanicked:
+			failureType = "Panic"
+		}
+		testCase.FailureMessage = &JUnitFailureMessage{
+			Type:    failureType,
+			Message: fmt.Sprintf("%s\n%s", exampleSummary.Failure.ComponentCodeLocation.String(), exampleSummary.Failure.Message),
+		}
+	}
+	if exampleSummary.State == types.ExampleStateSkipped || exampleSummary.State == types.ExampleStatePending {
+		testCase.Skipped = &JUnitSkipped{}
+	}
+	testCase.Time = exampleSummary.RunTime.Seconds()
+	reporter.suite.TestCases = append(reporter.suite.TestCases, testCase)
+}
+
+func (reporter *JUnitReporter) SpecSuiteDidEnd(summary *types.SuiteSummary) {
+	reporter.suite.Time = summary.RunTime.Seconds()
+	reporter.suite.Failures = summary.NumberOfFailedExamples
+	file, err := os.Create(reporter.filename)
+	if err != nil {
+		fmt.Printf("Failed to create JUnit report file: %s\n\t%s", reporter.filename, err.Error())
+	}
+	defer file.Close()
+	file.WriteString(xml.Header)
+	encoder := xml.NewEncoder(file)
+	encoder.Indent("  ", "    ")
+	err = encoder.Encode(reporter.suite)
+	if err != nil {
+		fmt.Printf("Failed to generate JUnit report\n\t%s", err.Error())
+	}
+}
---
+++ reporters/junit_reporter_test.go
@@ -0,0 +1,148 @@
+package reporters_test
+
+import (
+	"encoding/xml"
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
+	. "github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/ginkgo/types"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"time"
+)
+
+var _ = Describe("JUnit Reporter", func() {
+	var (
+		outputFile string
+		reporter   Reporter
+	)
+
+	readOutputFile := func() JUnitTestSuite {
+		bytes, err := ioutil.ReadFile(outputFile)
+		Ω(err).ShouldNot(HaveOccured())
+		var suite JUnitTestSuite
+		err = xml.Unmarshal(bytes, &suite)
+		Ω(err).ShouldNot(HaveOccured())
+		return suite
+	}
+
+	BeforeEach(func() {
+		outputFile = "/tmp/test.xml"
+		reporter = NewJUnitReporter(outputFile)
+
+		reporter.SpecSuiteWillBegin(config.GinkgoConfigType{}, &types.SuiteSummary{
+			SuiteDescription:              "My test suite",
+			NumberOfExamplesThatWillBeRun: 1,
+		})
+	})
+
+	Describe("a passing test", func() {
+		BeforeEach(func() {
+			example := &types.ExampleSummary{
+				ComponentTexts: []string{"[Top Level]", "A", "B", "C"},
+				State:          types.ExampleStatePassed,
+				RunTime:        5 * time.Second,
+			}
+			reporter.ExampleWillRun(example)
+			reporter.ExampleDidComplete(example)
+
+			reporter.SpecSuiteDidEnd(&types.SuiteSummary{
+				NumberOfExamplesThatWillBeRun: 1,
+				NumberOfFailedExamples:        0,
+				RunTime:                       10 * time.Second,
+			})
+		})
+
+		It("should record the test as passing", func() {
+			output := readOutputFile()
+			Ω(output.Tests).Should(Equal(1))
+			Ω(output.Failures).Should(Equal(0))
+			Ω(output.Time).Should(Equal(10.0))
+			Ω(output.TestCases).Should(HaveLen(1))
+			Ω(output.TestCases[0].Name).Should(Equal("A B C"))
+			Ω(output.TestCases[0].ClassName).Should(Equal("My test suite"))
+			Ω(output.TestCases[0].FailureMessage).Should(BeNil())
+			Ω(output.TestCases[0].Skipped).Should(BeNil())
+			Ω(output.TestCases[0].Time).Should(Equal(5.0))
+		})
+	})
+
+	exampleStateCases := []struct {
+		state   types.ExampleState
+		message string
+	}{
+		{types.ExampleStateFailed, "Failure"},
+		{types.ExampleStateTimedOut, "Timeout"},
+		{types.ExampleStatePanicked, "Panic"},
+	}
+
+	for _, exampleStateCase := range exampleStateCases {
+		exampleStateCase := exampleStateCase
+		Describe("a failing test", func() {
+			var example *types.ExampleSummary
+			BeforeEach(func() {
+				example = &types.ExampleSummary{
+					ComponentTexts: []string{"[Top Level]", "A", "B", "C"},
+					State:          exampleStateCase.state,
+					RunTime:        5 * time.Second,
+					Failure: types.ExampleFailure{
+						ComponentCodeLocation: types.GenerateCodeLocation(0),
+						Message:               "I failed",
+					},
+				}
+				reporter.ExampleWillRun(example)
+				reporter.ExampleDidComplete(example)
+
+				reporter.SpecSuiteDidEnd(&types.SuiteSummary{
+					NumberOfExamplesThatWillBeRun: 1,
+					NumberOfFailedExamples:        1,
+					RunTime:                       10 * time.Second,
+				})
+			})
+
+			It("should record test as failing", func() {
+				output := readOutputFile()
+				Ω(output.Tests).Should(Equal(1))
+				Ω(output.Failures).Should(Equal(1))
+				Ω(output.Time).Should(Equal(10.0))
+				Ω(output.TestCases[0].Name).Should(Equal("A B C"))
+				Ω(output.TestCases[0].ClassName).Should(Equal("My test suite"))
+				Ω(output.TestCases[0].FailureMessage.Type).Should(Equal(exampleStateCase.message))
+				Ω(output.TestCases[0].FailureMessage.Message).Should(ContainSubstring("I failed"))
+				Ω(output.TestCases[0].FailureMessage.Message).Should(ContainSubstring(example.Failure.ComponentCodeLocation.String()))
+				Ω(output.TestCases[0].Skipped).Should(BeNil())
+			})
+		})
+	}
+
+	for _, exampleStateCase := range []types.ExampleState{types.ExampleStatePending, types.ExampleStateSkipped} {
+		exampleStateCase := exampleStateCase
+		Describe("a skipped test", func() {
+			var example *types.ExampleSummary
+			BeforeEach(func() {
+				example = &types.ExampleSummary{
+					ComponentTexts: []string{"[Top Level]", "A", "B", "C"},
+					State:          exampleStateCase,
+					RunTime:        5 * time.Second,
+				}
+				reporter.ExampleWillRun(example)
+				reporter.ExampleDidComplete(example)
+
+				reporter.SpecSuiteDidEnd(&types.SuiteSummary{
+					NumberOfExamplesThatWillBeRun: 1,
+					NumberOfFailedExamples:        0,
+					RunTime:                       10 * time.Second,
+				})
+			})
+
+			It("should record test as failing", func() {
+				output := readOutputFile()
+				Ω(output.Tests).Should(Equal(1))
+				Ω(output.Failures).Should(Equal(0))
+				Ω(output.Time).Should(Equal(10.0))
+				Ω(output.TestCases[0].Name).Should(Equal("A B C"))
+				Ω(output.TestCases[0].Skipped).ShouldNot(BeNil())
+			})
+		})
+	}
+})
