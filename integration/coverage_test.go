3332369fdb610eb5198ff3b98b8eb028dfb94e39
johnnadratowski <john@unifiedsocial.com>
2015-05-11 16:48:35 -0400

    Whoops, forgot to cleanup test

--- integration/coverage_test.go
+++ integration/coverage_test.go
@@ -1,6 +1,7 @@
 package integration_test
 
 import (
+	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
@@ -10,7 +11,7 @@ import (
 
 var _ = Describe("Coverage Specs", func() {
 	AfterEach(func() {
-		//os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
+		os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
 	})
 
 	It("runs coverage analysis in series and in parallel", func() {
@@ -22,7 +23,7 @@ var _ = Describe("Coverage Specs", func() {
 		serialCoverProfileOutput, err := exec.Command("go", "tool", "cover", "-func=./_fixtures/coverage_fixture/coverage_fixture.coverprofile").CombinedOutput()
 		Ω(err).ShouldNot(HaveOccurred())
 
-		//os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
+		os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
 
 		Eventually(startGinkgo("./_fixtures/coverage_fixture", "-cover", "-nodes=4")).Should(gexec.Exit(0))
 
@@ -32,7 +33,7 @@ var _ = Describe("Coverage Specs", func() {
 		Ω(parallelCoverProfileOutput).Should(Equal(serialCoverProfileOutput))
 	})
 
-	FIt("runs coverage analysis on external packages in series and in parallel", func() {
+	It("runs coverage analysis on external packages in series and in parallel", func() {
 		session := startGinkgo("./_fixtures/coverage_fixture", "-coverpkg=github.com/onsi/ginkgo/integration/_fixtures/coverage_fixture,github.com/onsi/ginkgo/integration/_fixtures/coverage_fixture/external_coverage_fixture")
 		Eventually(session).Should(gexec.Exit(0))
 		output := session.Out.Contents()
@@ -41,7 +42,7 @@ var _ = Describe("Coverage Specs", func() {
 		serialCoverProfileOutput, err := exec.Command("go", "tool", "cover", "-func=./_fixtures/coverage_fixture/coverage_fixture.coverprofile").CombinedOutput()
 		Ω(err).ShouldNot(HaveOccurred())
 
-		//os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
+		os.RemoveAll("./_fixtures/coverage_fixture/coverage_fixture.coverprofile")
 
 		Eventually(startGinkgo("./_fixtures/coverage_fixture", "-coverpkg=github.com/onsi/ginkgo/integration/_fixtures/coverage_fixture,github.com/onsi/ginkgo/integration/_fixtures/coverage_fixture/external_coverage_fixture", "-nodes=4")).Should(gexec.Exit(0))
 
