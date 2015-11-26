4e84bbfb7d968ebd967a58e4af1015c9d8dc5275
Dave Protasowski <dprotaso@gmail.com>
2015-11-25 19:01:00 -0500

    ginkgo unfocus works for DescribeTable and Entry
    Signed-off-by: Ergin Babani <ebabani@pivotal.io>

--- integration/_fixtures/focused_fixture/focused_fixture_test.go
+++ integration/_fixtures/focused_fixture/focused_fixture_test.go
@@ -2,6 +2,7 @@ package focused_fixture_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 )
 
 var _ = Describe("FocusedFixture", func() {
@@ -25,6 +26,16 @@ var _ = Describe("FocusedFixture", func() {
 
 	}, 2)
 
+	FDescribeTable("focused",
+		func() {},
+		Entry("focused"),
+	)
+
+	DescribeTable("focused",
+		func() {},
+		FEntry("focused"),
+	)
+
 	Describe("not focused", func() {
 		It("not focused", func() {
 
@@ -44,4 +55,9 @@ var _ = Describe("FocusedFixture", func() {
 	Measure("not focused", func(b Benchmarker) {
 
 	}, 2)
+
+	DescribeTable("not focused",
+		func() {},
+		Entry("not focused"),
+	)
 })
