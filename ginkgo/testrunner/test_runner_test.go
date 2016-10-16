afe049e1120cfc831ad789f4895818d3c6c95de8
Onsi Fakhouri <onsijoe@gmail.com>
2016-10-16 13:59:02 -0700

    remove -i in invocations of go test.  fixes #305

--- ginkgo/testrunner/test_runner_test.go
+++ ginkgo/testrunner/test_runner_test.go
@@ -1,11 +1,12 @@
 package testrunner_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/ginkgo/testrunner"
 	"github.com/onsi/ginkgo/ginkgo/testsuite"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
 func strAddr(s string) interface{} {
@@ -38,7 +39,6 @@ var _ = Describe("TestRunner", func() {
 		Ω(args).Should(Equal([]string{
 			"test",
 			"-c",
-			"-i",
 			"-o",
 			".",
 			"",
