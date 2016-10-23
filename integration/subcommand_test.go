a8ccd817611666211fdf6e6a752174a0db8374a8
Ian McCracken <ian.mccracken@gmail.com>
2016-10-23 15:30:19 -0500

    Add ability to specify a custom bootstrap file (#302)
    * Add ability to specify a custom bootstrap file
    
    * Add integration test for bootstrap --template
    
    * Make the bootstrap template valid Go so go fmt doesn't choke

--- integration/subcommand_test.go
+++ integration/subcommand_test.go
@@ -82,6 +82,36 @@ var _ = Describe("Subcommand", func() {
 			Ω(content).Should(ContainSubstring("\t" + `. "github.com/onsi/gomega"`))
 			Ω(content).Should(ContainSubstring("\t" + `"github.com/sclevine/agouti"`))
 		})
+
+		It("should generate a bootstrap file using a template when told to", func() {
+			templateFile := filepath.Join(pkgPath, ".bootstrap")
+			ioutil.WriteFile(templateFile, []byte(`package {{.Package}}
+
+			import (
+				{{.GinkgoImport}}
+				{{.GomegaImport}}
+
+				"testing"
+				"binary"
+			)
+
+			func Test{{.FormattedName}}(t *testing.T) {
+				// This is a {{.Package}} test
+			}`), 0666)
+			session := startGinkgo(pkgPath, "bootstrap", "--template", templateFile)
+			Eventually(session).Should(gexec.Exit(0))
+			output := session.Out.Contents()
+
+			Ω(output).Should(ContainSubstring("foo_suite_test.go"))
+
+			content, err := ioutil.ReadFile(filepath.Join(pkgPath, "foo_suite_test.go"))
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(content).Should(ContainSubstring("package foo_test"))
+			Ω(content).Should(ContainSubstring(`. "github.com/onsi/ginkgo"`))
+			Ω(content).Should(ContainSubstring(`. "github.com/onsi/gomega"`))
+			Ω(content).Should(ContainSubstring(`"binary"`))
+			Ω(content).Should(ContainSubstring("// This is a foo_test test"))
+		})
 	})
 
 	Describe("nodot", func() {
