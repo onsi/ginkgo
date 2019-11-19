9e98a7bb935148c93463216a698396632c17b013
Mohammed Al Sahaf <msaa1990@gmail.com>
2019-11-19 04:13:58 +0300

    use pipes to redirect output instead of a file

--- internal/remote/output_interceptor.go
+++ internal/remote/output_interceptor.go
@@ -22,7 +22,8 @@ func NewOutputInterceptor() OutputInterceptor {
 }
 
 type outputInterceptor struct {
-	redirectFile *os.File
+	readPipe     *os.File
+	writePipe    *os.File
 	streamTarget *os.File
 	intercepting bool
 	tailer       io.Reader
@@ -34,22 +35,14 @@ func (interceptor *outputInterceptor) StartInterceptingOutput() error {
 	}
 	interceptor.intercepting = true
 
-	var err error
-
-	interceptor.redirectFile, err = ioutil.TempFile("", "ginkgo-output")
-	if err != nil {
-		return err
-	}
-
-	// Call a function in ./syscall_dup_*.go
-	// If building for everything other than linux_arm64,
-	// use a "normal" syscall.Dup2(oldfd, newfd) call. If building for linux_arm64 (which doesn't have syscall.Dup2)
-	// call syscall.Dup3(oldfd, newfd, 0). They are nearly identical, see: http://linux.die.net/man/2/dup3
-	syscallDup(int(interceptor.redirectFile.Fd()), 1)
-	syscallDup(int(interceptor.redirectFile.Fd()), 2)
-
 	if interceptor.streamTarget != nil {
-		interceptor.tailer = io.TeeReader(interceptor.redirectFile, interceptor.streamTarget)
+		r, w, err := os.Pipe()
+		if err != nil {
+			return err
+		}
+		interceptor.readPipe = r
+		interceptor.writePipe = w
+		interceptor.tailer = io.TeeReader(r, w)
 	}
 
 	return nil
@@ -60,9 +53,9 @@ func (interceptor *outputInterceptor) StopInterceptingAndReturnOutput() (string,
 		return "", errors.New("Not intercepting output!")
 	}
 
-	interceptor.redirectFile.Close()
-	output, err := ioutil.ReadFile(interceptor.redirectFile.Name())
-	os.Remove(interceptor.redirectFile.Name())
+	output, err := ioutil.ReadAll(interceptor.tailer)
+	interceptor.writePipe.Close()
+	interceptor.readPipe.Close()
 
 	interceptor.intercepting = false
 
