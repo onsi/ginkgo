9cd84bd31058aef20026fce9411bf765ae240ca9
Mohammed Al Sahaf <msaa1990@gmail.com>
2019-11-19 03:00:00 +0300

    remove dependency on github.com/hpcloud/tail by using io.TeeReader

--- internal/remote/output_interceptor.go
+++ internal/remote/output_interceptor.go
@@ -1,6 +1,11 @@
 package remote
 
-import "os"
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+)
 
 /*
 The OutputInterceptor is used by the ForwardingReporter to
@@ -10,4 +15,64 @@ type OutputInterceptor interface {
 	StartInterceptingOutput() error
 	StopInterceptingAndReturnOutput() (string, error)
 	StreamTo(*os.File)
+}
+
+func NewOutputInterceptor() OutputInterceptor {
+	return &outputInterceptor{}
+}
+
+type outputInterceptor struct {
+	redirectFile *os.File
+	streamTarget *os.File
+	intercepting bool
+	tailer       io.Reader
+}
+
+func (interceptor *outputInterceptor) StartInterceptingOutput() error {
+	if interceptor.intercepting {
+		return errors.New("Already intercepting output!")
+	}
+	interceptor.intercepting = true
+
+	var err error
+
+	interceptor.redirectFile, err = ioutil.TempFile("", "ginkgo-output")
+	if err != nil {
+		return err
+	}
+
+	// Call a function in ./syscall_dup_*.go
+	// If building for everything other than linux_arm64,
+	// use a "normal" syscall.Dup2(oldfd, newfd) call. If building for linux_arm64 (which doesn't have syscall.Dup2)
+	// call syscall.Dup3(oldfd, newfd, 0). They are nearly identical, see: http://linux.die.net/man/2/dup3
+	syscallDup(int(interceptor.redirectFile.Fd()), 1)
+	syscallDup(int(interceptor.redirectFile.Fd()), 2)
+
+	if interceptor.streamTarget != nil {
+		interceptor.tailer = io.TeeReader(interceptor.redirectFile, interceptor.streamTarget)
+	}
+
+	return nil
+}
+
+func (interceptor *outputInterceptor) StopInterceptingAndReturnOutput() (string, error) {
+	if !interceptor.intercepting {
+		return "", errors.New("Not intercepting output!")
+	}
+
+	interceptor.redirectFile.Close()
+	output, err := ioutil.ReadFile(interceptor.redirectFile.Name())
+	os.Remove(interceptor.redirectFile.Name())
+
+	interceptor.intercepting = false
+
+	if interceptor.streamTarget != nil {
+		interceptor.streamTarget.Sync()
+	}
+
+	return string(output), err
+}
+
+func (interceptor *outputInterceptor) StreamTo(out *os.File) {
+	interceptor.streamTarget = out
 }
