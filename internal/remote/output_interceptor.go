37c24267450bf626db78bc67d2c96941557baf18
Mohammed Al Sahaf <msaa1990@gmail.com>
2019-11-22 21:57:17 +0300

    trial: a hack to intercept Stdou/Stderr by replacing the variables in os.*

--- internal/remote/output_interceptor.go
+++ internal/remote/output_interceptor.go
@@ -1,6 +1,7 @@
 package remote
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"io/ioutil"
@@ -18,26 +19,46 @@ type OutputInterceptor interface {
 }
 
 func NewOutputInterceptor() OutputInterceptor {
-	return &outputInterceptor{}
+	return &outputInterceptor{buffer: &bytes.Buffer{}}
 }
 
 type outputInterceptor struct {
+	origStdout     *os.File
+	origStderr     *os.File
+	readSources    [2]io.ReadCloser // stores the reader pipes form os.Pipe() for closure.
 	streamTarget   *os.File
 	combinedReader io.Reader
 	intercepting   bool
 	tailer         io.Reader
+	buffer         *bytes.Buffer
 }
 
 func (interceptor *outputInterceptor) StartInterceptingOutput() error {
 	if interceptor.intercepting {
 		return errors.New("Already intercepting output!")
 	}
-	interceptor.intercepting = true
-	interceptor.combinedReader = io.MultiReader(os.Stdout, os.Stderr)
-
-	if interceptor.streamTarget != nil {
-		interceptor.tailer = io.TeeReader(interceptor.combinedReader, interceptor.streamTarget)
+	interceptor.origStdout = os.Stdout
+	interceptor.origStderr = os.Stderr
+	stdoutRead, stdoutWrite, err := os.Pipe()
+	if err != nil {
+		return err
 	}
+	stderrRead, stderrWrite, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+
+	os.Stdout = stdoutWrite
+	os.Stderr = stderrWrite
+
+	interceptor.intercepting = true
+
+	interceptor.readSources = [2]io.ReadCloser{stderrRead, stderrRead}
+	interceptor.combinedReader = io.TeeReader(io.MultiReader(stderrRead, stdoutRead), interceptor.buffer)
+
+	// if interceptor.streamTarget != nil {
+	// 	interceptor.tailer = io.TeeReader(interceptor.combinedReader, interceptor.streamTarget)
+	// }
 
 	return nil
 }
@@ -47,13 +68,27 @@ func (interceptor *outputInterceptor) StopInterceptingAndReturnOutput() (string,
 		return "", errors.New("Not intercepting output!")
 	}
 
-	output, err := ioutil.ReadAll(interceptor.combinedReader)
+	output, err := ioutil.ReadAll(interceptor.buffer)
+
+	currStdout := os.Stdout
+	currStdout.Close()
+	currStderr := os.Stderr
+	currStderr.Close()
+
+	os.Stdout = interceptor.origStdout
+	os.Stderr = interceptor.origStderr
+
+	for _, r := range interceptor.readSources {
+		if closerErr := r.Close(); closerErr != nil {
+			err = closerErr
+		}
+	}
 
 	interceptor.intercepting = false
 
-	if interceptor.streamTarget != nil {
-		interceptor.streamTarget.Sync()
-	}
+	// if interceptor.streamTarget != nil {
+	// 	interceptor.streamTarget.Sync()
+	// }
 
 	return string(output), err
 }
