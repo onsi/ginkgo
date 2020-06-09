21eaef25591057b6c6e6d4821ff1126bf06b3781
Federico Paolinelli <fpaoline@redhat.com>
2020-06-09 18:39:46 +0200

    Add a version of table.Entry that allows dumping the entry parameters. (#689)
    Sometimes, it's useful to have the parameters of a given table entry as part of the description of the It counterpart.
    To achieve that, we allow passing a function as the description parameter.
    In case a string is passed, the previous behaviour is preserved. In case of function, it is expected to return a string
    that will be used as the new description for the generated It.

--- extensions/table/table_test.go
+++ extensions/table/table_test.go
@@ -1,6 +1,7 @@
 package table_test
 
 import (
+	"fmt"
 	"strings"
 
 	. "github.com/onsi/ginkgo/extensions/table"
@@ -60,5 +61,55 @@ var _ = Describe("Table", func() {
 			Expect(x).To(BeNil())
 		},
 		Entry("nil", nil),
+	)
+})
+
+var _ = Describe("TableWithParametricDescription", func() {
+	describe := func(desc string) func(int, int, bool) string {
+		return func(x, y int, expected bool) string {
+			return fmt.Sprintf("%s x=%d y=%d expected:%t", desc, x, y, expected)
+		}
+	}
+
+	DescribeTable("a simple table",
+		func(x int, y int, expected bool) {
+			Ω(x > y).Should(Equal(expected))
+		},
+		Entry(describe("x > y"), 1, 0, true),
+		Entry(describe("x == y"), 0, 0, false),
+		Entry(describe("x < y"), 0, 1, false),
+	)
+
+	type ComplicatedThings struct {
+		Superstructure string
+		Substructure   string
+		Count          int
+	}
+
+	describeComplicated := func(desc string) func(ComplicatedThings) string {
+		return func(things ComplicatedThings) string {
+			return fmt.Sprintf("%s things=%v", desc, things)
+		}
+	}
+
+	DescribeTable("a more complicated table",
+		func(c ComplicatedThings) {
+			Ω(strings.Count(c.Superstructure, c.Substructure)).Should(BeNumerically("==", c.Count))
+		},
+		Entry(describeComplicated("with no matching substructures"), ComplicatedThings{
+			Superstructure: "the sixth sheikh's sixth sheep's sick",
+			Substructure:   "emir",
+			Count:          0,
+		}),
+		Entry(describeComplicated("with one matching substructure"), ComplicatedThings{
+			Superstructure: "the sixth sheikh's sixth sheep's sick",
+			Substructure:   "sheep",
+			Count:          1,
+		}),
+		Entry(describeComplicated("with many matching substructures"), ComplicatedThings{
+			Superstructure: "the sixth sheikh's sixth sheep's sick",
+			Substructure:   "si",
+			Count:          3,
+		}),
 	)
 })
