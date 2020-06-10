fc0e44ebb322aef86fc01a7da058ee418d3e6a31
Jian Zhang <jianzhangbjz@outlook.com>
2020-06-11 00:21:41 +0800

    add books package (#568)

---
+++ example/books/books_suite_test.go
@@ -0,0 +1,13 @@
+package books_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestBooks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Books Suite")
+}
