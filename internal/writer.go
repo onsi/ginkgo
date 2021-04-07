f0c592833cdb14dbcf21c0d5bd36f8a2d229f01d
Onsi Fakhouri <onsijoe@gmail.com>
2021-04-06 20:29:00 -0600

    Expand GinkgoWriter functionality
    - GinkgoWriter now provides `GinkgoWriter.Print(...)`, `GinkgoWriter.Printf(...)`, and `GinkgoWriter.Println(...)` convenience methods.
    - GinkgoWriter can now tee to additional writers via `GinkgoWriter.TeeTo(writer)`

--- internal/writer.go
+++ internal/writer.go
@@ -2,8 +2,16 @@ package internal
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"sync"
+)
+
+type WriterMode uint
+
+const (
+	WriterModeStreamWithoutBuffer WriterMode = iota
+	WriterModeBufferOnly
 )
 
 type WriterInterface interface {
@@ -13,11 +21,14 @@ type WriterInterface interface {
 	Bytes() []byte
 }
 
+//Writer impplements WriterInterface and GinkgoWriterInterface
 type Writer struct {
 	buffer    *bytes.Buffer
 	outWriter io.Writer
 	lock      *sync.Mutex
-	stream    bool
+	mode      WriterMode
+
+	teeWriters []io.Writer
 }
 
 func NewWriter(outWriter io.Writer) *Writer {
@@ -25,24 +36,29 @@ func NewWriter(outWriter io.Writer) *Writer {
 		buffer:    &bytes.Buffer{},
 		lock:      &sync.Mutex{},
 		outWriter: outWriter,
-		stream:    true,
+		mode:      WriterModeStreamWithoutBuffer,
 	}
 }
 
-func (w *Writer) SetStream(stream bool) {
+func (w *Writer) SetMode(mode WriterMode) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
-	w.stream = stream
+	w.mode = mode
 }
 
 func (w *Writer) Write(b []byte) (n int, err error) {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 
-	if w.stream {
-		return w.outWriter.Write(b)
+	for _, teeWriter := range w.teeWriters {
+		teeWriter.Write(b)
 	}
-	return w.buffer.Write(b)
+
+	if w.mode == WriterModeStreamWithoutBuffer {
+		return w.outWriter.Write(b)
+	} else {
+		return w.buffer.Write(b)
+	}
 }
 
 func (w *Writer) Truncate() {
@@ -58,4 +74,31 @@ func (w *Writer) Bytes() []byte {
 	copied := make([]byte, len(b))
 	copy(copied, b)
 	return copied
+}
+
+//GinkgoWriterInterface
+func (w *Writer) TeeTo(writer io.Writer) {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	w.teeWriters = append(w.teeWriters, writer)
+}
+
+func (w *Writer) ClearTeeWriters() {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	w.teeWriters = []io.Writer{}
+}
+
+func (w *Writer) Print(a ...interface{}) {
+	fmt.Fprint(w, a...)
+}
+
+func (w *Writer) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(w, format, a...)
+}
+
+func (w *Writer) Println(a ...interface{}) {
+	fmt.Fprintln(w, a...)
 }
