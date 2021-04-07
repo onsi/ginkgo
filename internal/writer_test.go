497a9375103e2ec4b00972504e50b7292bc5a23c
Onsi Fakhouri <onsijoe@gmail.com>
2021-04-06 20:29:00 -0600

    Expand GinkgoWriter functionality
    - GinkgoWriter now provides `GinkgoWriter.Print(...)`, `GinkgoWriter.Printf(...)`, and `GinkgoWriter.Println(...)` convenience methods.
    - GinkgoWriter can now tee to additional writers via `GinkgoWriter.TeeTo(writer)`

--- internal/writer_test.go
+++ internal/writer_test.go
@@ -17,7 +17,7 @@ var _ = Describe("Writer", func() {
 		writer = internal.NewWriter(out)
 	})
 
-	Context("when configured to stream (the default)", func() {
+	Context("when configured to WriterModeStreamWithoutBuffer", func() {
 		It("should stream directly to the passed in writer by default", func() {
 			writer.Write([]byte("foo"))
 			Ω(out).Should(gbytes.Say("foo"))
@@ -30,9 +30,9 @@ var _ = Describe("Writer", func() {
 		})
 	})
 
-	Context("when told not to stream", func() {
+	Context("when configured to WriterModeBufferOnly", func() {
 		BeforeEach(func() {
-			writer.SetStream(false)
+			writer.SetMode(internal.WriterModeBufferOnly)
 		})
 
 		It("should not write to the passed in writer", func() {
@@ -55,6 +55,57 @@ var _ = Describe("Writer", func() {
 				writer.Write([]byte("bar"))
 				Ω(writer.Bytes()).Should(Equal([]byte("bar")))
 			})
+		})
+	})
+
+	Describe("Teeing to additional writers", func() {
+		var tee1, tee2 *gbytes.Buffer
+		BeforeEach(func() {
+			tee1 = gbytes.NewBuffer()
+			tee2 = gbytes.NewBuffer()
+		})
+
+		Context("when told to tee to additional writers", func() {
+			BeforeEach(func() {
+				writer.TeeTo(tee1)
+				writer.TeeTo(tee2)
+			})
+
+			It("tees to all registered tee writers", func() {
+				writer.Print("hello")
+				Ω(string(tee1.Contents())).Should(Equal("hello"))
+				Ω(string(tee2.Contents())).Should(Equal("hello"))
+			})
+
+			Context("when told to clear tee writers", func() {
+				BeforeEach(func() {
+					writer.ClearTeeWriters()
+				})
+
+				It("stops teeing to said writers", func() {
+					writer.Print("goodbye")
+					Ω(tee1.Contents()).Should(BeEmpty())
+					Ω(tee2.Contents()).Should(BeEmpty())
+				})
+			})
+
+		})
+	})
+
+	Describe("Convenience print methods", func() {
+		It("can Print", func() {
+			writer.Print("foo", "baz", " ", "bizzle")
+			Ω(string(out.Contents())).Should(Equal("foobaz bizzle"))
+		})
+
+		It("can Println", func() {
+			writer.Println("foo", "baz", " ", "bizzle")
+			Ω(string(out.Contents())).Should(Equal("foo baz   bizzle\n"))
+		})
+
+		It("can Printf", func() {
+			writer.Printf("%s%d - %s\n", "foo", 17, "bar")
+			Ω(string(out.Contents())).Should(Equal("foo17 - bar\n"))
 		})
 	})
 })
