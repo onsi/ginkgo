61085be918d699595c339598c8bc7f3d157c54dd
Onsi Fakhouri <onsijoe@gmail.com>
2021-05-06 11:05:23 -0600

    Implement best-effort OutputInterceptor for windows
    The unix OutputInterceptor leverages sycall.Dup2 to shunt all output bound for Stdout and Stderr over to a buffer.  This syscall does not exist on windows.  Instead we set os.Stdout and os.Stderr to pipe writers on windows in the hope that most code emitting output is using those globals.
    
    This commit also changes the unix implementation to use an in-memory pipe as opposed to a temporary file.  It looks like this has won some small performance gains.

--- internal/output_interceptor_win.go
+++ internal/output_interceptor_win.go
@@ -1,7 +1,63 @@
 // +build windows
 
-package parallel_support
+package internal
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
 
 func NewOutputInterceptor() OutputInterceptor {
-	return noopOutputInterceptor{}
+	return &osGlobalReassigningOutputInterceptor{
+		interceptedContent: make(chan string),
+	}
+}
+
+type osGlobalReassigningOutputInterceptor struct {
+	intercepting bool
+
+	originalStdout *os.File
+	originalStderr *os.File
+
+	interceptingWriter io.Closer
+	interceptedContent chan string
+}
+
+func (interceptor *osGlobalReassigningOutputInterceptor) StartInterceptingOutput() {
+	if interceptor.intercepting {
+		return
+	}
+	interceptor.intercepting = true
+
+	interceptor.originalStdout = os.Stdout
+	interceptor.originalStderr = os.Stderr
+
+	reader, writer, _ := os.Pipe()
+	interceptor.interceptingWriter = writer
+
+	go func() {
+		buffer := &bytes.Buffer{}
+		io.Copy(buffer, reader)
+		interceptor.interceptedContent <- buffer.String()
+	}()
+
+	os.Stdout = writer
+	os.Stderr = writer
+}
+
+func (interceptor *osGlobalReassigningOutputInterceptor) StopInterceptingAndReturnOutput() string {
+	if !interceptor.intercepting {
+		return ""
+	}
+
+	os.Stdout = interceptor.originalStdout
+	os.Stderr = interceptor.originalStderr
+
+	interceptor.interceptingWriter.Close()
+	content := <-interceptor.interceptedContent
+
+	interceptor.intercepting = false
+
+	return content
 }
