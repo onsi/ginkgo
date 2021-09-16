5e5454a7d4475d751596201a6487c86cc6ade685
Onsi Fakhouri <onsijoe@gmail.com>
2021-09-16 12:14:32 -0600

    Add Ordered decorator - currently a no-op

--- decorator_dsl.go
+++ decorator_dsl.go
@@ -17,7 +17,12 @@ const Focus = internal.Focus
 const Pending = internal.Pending
 
 //Serial is a decorator that allows you to mark a test or container as serial.  These tests will never run in parallel with other tests.
+//Tests in ordered containers cannot be marked as serial - mark the ordered container instead.
 const Serial = internal.Serial
+
+//Ordered is a decorator that allows you to mark a container as ordered.  Tests in the container will always run in the order they appear.
+// They will never be randomized and they will never run in parallel with one another, though they may run in parallel with other tests.
+const Ordered = internal.Ordered
 
 //Label decorates specs with Labels.  Multiple labels can be passed to Label and these can be arbitrary strings but must not include the following characters: "&|!,()/".
 //Labels can be pplied to container and test nodes, but not setup nodes.  You can provide multiple Labels to a given node and a spec's labels is the union of all labels in its node hierarchy.
