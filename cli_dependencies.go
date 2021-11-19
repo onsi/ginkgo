c2cf60c8b53fc568e164969557080f4fe63830e5
Onsi Fakhouri <onsijoe@gmail.com>
2021-11-18 20:23:12 -0700

    remove cli dependencies hack

--- cli_dependencies.go
+++
@@ -1,9 +0,0 @@
-package ginkgo
-
-import (
-	_ "github.com/go-task/slim-sprig"
-	_ "github.com/google/pprof/profile"
-	_ "golang.org/x/tools/go/ast/inspector"
-)
-
-// This file imports the CLI dependencies so that consuming packages have all the dependenceies they need to compile and run the Ginkgo CLI
