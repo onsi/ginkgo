fee691ccda81448f3fb7e64c9d2190afa9350eed
Brad P. Crochet <brad@redhat.com>
2022-09-26 11:52:19 -0400

    Make the outline command able to use the DSL import
    The outline command throws an error if the only the core DSL is
    imported. This allows any of the DSL to be imported and still
    produce a valid outline.
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

---
+++ ginkgo/outline/_testdata/dsl_core_test.go
@@ -0,0 +1,14 @@
+package example_test
+
+import (
+	. "github.com/onsi/ginkgo/v2/dsl/core"
+)
+
+var _ = Describe("DslCoreFixture", func() {
+	Describe("dslcore", func() {
+		It("dslcore", func() {
+			By("step 1")
+			By("step 2")
+		})
+	})
+})
