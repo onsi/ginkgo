e6e3b989075c2a9e645402aee442976a327e0397
Danny Baez <danny.baez.jr@gmail.com>
2023-01-05 11:40:31 -0500

    Support for labels and pending decorator in ginkgo outline output (#1113)
    * ginkgo outline will now display a Labels column/field with
       labels assigned to nodes
    
     * proper support for the pending decorator assigned to nodes
    
     * documentation update
    
     * update to outline tests

---
+++ ginkgo/outline/_testdata/labels_test.go
@@ -0,0 +1,44 @@
+package example_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("NormalFixture", func() {
+	Describe("normal", Label("normal", "serial"), func() {
+		It("normal", func() {
+			By("step 1")
+			By("step 2")
+		})
+	})
+
+	Context("normal", func() {
+		It("normal", Label("medium"), Label("slow"), func() {
+
+		})
+	})
+
+	When("normal", func() {
+		It("normal", func() {
+
+		})
+	})
+
+	It("normal", func() {
+
+	})
+
+	Specify("normal", func() {
+
+	})
+
+	DescribeTable("normal",
+		func() {},
+		Entry("normal"),
+	)
+
+	DescribeTable("normal",
+		func() {},
+		Entry("normal"),
+	)
+})
