baa6a8e63b45cb8601b6884a33ab831c5d2965c5
Rudolph Jacobs <rudolph@kalido.me>
2025-01-21 17:40:32 +0200

    Fix binary paths when precompiling multiple suites.

---
+++ integration/_fixtures/build_reporting_fixture/suite2/build_reporting_2_suite_test.go
@@ -0,0 +1,13 @@
+package suite2_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestAfterRunHook(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Build reporting suite 1")
+}
