4df06c6d3b5480f739fd1b3cefaa7e241d8b3a31
Rudolph Jacobs <rudolph@kalido.me>
2025-01-21 17:40:32 +0200

    Fix binary paths when precompiling multiple suites.

---
+++ integration/_fixtures/build_reporting_fixture/suite2/build_reporting_2_test.go
@@ -0,0 +1,18 @@
+package suite2_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Testing", func() {
+	It("it should succeed", func() {
+		Ω(true).Should(Equal(true))
+	})
+
+	PIt("a failing test", func() {
+		It("should fail", func() {
+			Ω(true).Should(Equal(false))
+		})
+	})
+})
