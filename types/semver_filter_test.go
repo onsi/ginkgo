71d2d89adc9387d4f4fc579438b5631d9180d687
Icarus Wu <icaruswu66@qq.com>
2026-01-11 22:44:15 +0800

    feat: support component semantic version filtering
    Signed-off-by: Icarus Wu <icaruswu66@qq.com>

--- types/semver_filter_test.go
+++ types/semver_filter_test.go
@@ -23,31 +23,146 @@ var _ = Describe("SemVerFilter", func() {
 		Entry("valid constraint", "2.x.0", ""),
 	)
 
-	DescribeTable("ParseSemVerFilter", func(version string, constraints []string, expectedErrMsg string, shouldPass bool) {
-		filterFn, err := types.ParseSemVerFilter(version)
-		if expectedErrMsg != "" {
-			Expect(err.Error()).Should(ContainSubstring(expectedErrMsg))
+	type input struct {
+		version        string
+		component      string
+		constraints    []string
+		expectedErrMsg string
+		shouldPass     bool
+	}
+	DescribeTable("ParseSemVerFilter", func(i input) {
+		filterFn, err := types.ParseSemVerFilter(i.version)
+		if i.expectedErrMsg != "" {
+			Expect(err.Error()).Should(ContainSubstring(i.expectedErrMsg))
 			return
 		}
 		Expect(err).ShouldNot(HaveOccurred())
-		Expect(filterFn(constraints)).To(Equal(shouldPass))
+		Expect(filterFn(i.component, i.constraints)).To(Equal(i.shouldPass))
 	},
-		Entry("no semantic version filter", "", []string{"> 1.0.0"}, "", true),
-		Entry("no semantic version constraints", "2.0.0", []string{}, "", true),
-		Entry("invalid semantic version filter", "a1.0.0", []string{"> 1.0.0"}, "invalid filter version", false),
-		Entry("matched semantic version with single constraint", "2.0.0", []string{"> 1.0.0"}, "", true),
-		Entry("matched semantic version with multiple constraints", "2.0.0", []string{"> 1.0.0", "< 3.0.0"}, "", true),
-		Entry("matched semantic version with complex constraint", "2.0.0", []string{"> 1.0.0, < 3.0.0"}, "", true),
-		Entry("unmatched semantic version with constraint", "0.1.0", []string{"> 1.0.0, < 3.0.0"}, "", false),
+		Entry("no semantic version filter for constraints", input{
+			version:        "",
+			component:      "",
+			constraints:    []string{"> 1.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("no semantic version filter for component constraints", input{
+			version:        "",
+			component:      "componentA",
+			constraints:    []string{"> 1.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("no semantic version constraints", input{
+			version:        "2.0.0",
+			component:      "",
+			constraints:    []string{},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("no semantic version constraints for a component", input{
+			version:        "2.0.0",
+			component:      "componentA",
+			constraints:    []string{},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("invalid semantic version filter", input{
+			version:        "a1.0.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0"},
+			expectedErrMsg: "invalid filter version",
+			shouldPass:     false,
+		}),
+		Entry("matched semantic version with single constraint", input{
+			version:        "2.0.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched semantic version with component single constraint", input{
+			version:        "compA=2.0.0",
+			component:      "compA",
+			constraints:    []string{"> 1.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched semantic version with multiple constraints", input{
+			version:        "2.0.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0", "< 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched semantic version with component multiple constraints", input{
+			version:        "compA=2.0.0",
+			component:      "compA",
+			constraints:    []string{"> 1.0.0", "< 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("ignore non-component semantic version with component constraint", input{
+			version:        "2.0.0",
+			component:      "compA",
+			constraints:    []string{"== 1.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched semantic version with complex constraint", input{
+			version:        "2.0.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched semantic version with component complex constraint", input{
+			version:        "compA=2.0.0",
+			component:      "compA",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched mixed semantic version with constraints", input{
+			version:        "1.2.0, compA=2.0.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("matched mixed semantic version with component constraints", input{
+			version:        "1.0.0, compA=2.0.0",
+			component:      "compA",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     true,
+		}),
+		Entry("unmatched semantic version with constraint", input{
+			version:        "0.1.0",
+			component:      "",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     false,
+		}),
+		Entry("unmatched component semantic version with component constraint", input{
+			version:        "compA=0.1.0",
+			component:      "compA",
+			constraints:    []string{"> 1.0.0, < 3.0.0"},
+			expectedErrMsg: "",
+			shouldPass:     false,
+		}),
 	)
 
 	Describe("MustParseSemVerFilter", func() {
 		It("panics if passed an invalid filter", func() {
-			Ω(types.MustParseSemVerFilter("1.2.3")([]string{"> 1.0.0"})).Should(BeTrue())
-			Ω(types.MustParseSemVerFilter("3.2.0")([]string{"> 1.0.0", "< 2.0.0"})).Should(BeFalse())
+			Ω(types.MustParseSemVerFilter("1.2.3")("", []string{"> 1.0.0"})).Should(BeTrue())
+			Ω(types.MustParseSemVerFilter("3.2.0")("", []string{"> 1.0.0", "< 2.0.0"})).Should(BeFalse())
 			Ω(func() {
 				types.MustParseSemVerFilter("a1.2.3")
 			}).Should(Panic())
+			Ω(types.MustParseSemVerFilter("compA=1.2.3")("compA", []string{"> 1.0.0"})).Should(BeTrue())
+			Ω(types.MustParseSemVerFilter("compA=3.2.0")("compA", []string{"> 1.0.0", "< 2.0.0"})).Should(BeFalse())
+			Ω(types.MustParseSemVerFilter("1.0.0")("compA", []string{"> 2.0.0"})).Should(BeTrue())
 		})
 	})
 })
